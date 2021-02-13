9b3808a5a7ec05b6d22bdce95eef8ddaa55c78e3
carlotapg <carlota.pg@hotmail.com>
2021-02-13 13:28:57 +0100

    first private routes

--- routes/private.js
+++ routes/private.js
@@ -1,28 +1,61 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const privateRouter = express.Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/user-model");
+const zxcvbn = require("zxcvbn");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-
+//custom middleware
+function loggedIn(req, res, next) {
+  if (req.session.currentUser) {
+    next();
+  } else {
+    res.redirect("/");
+  }
+}
 
 //GET /PRIVATE/PROFILE renders personal profile view
 
+privateRouter.get("/myprofile", loggedIn, (req, res, next) => {
+  res.render("myprofile");
+});
+
 //GET /PRIVATE/EDIT-PROFILE renders edit-profile view
+privateRouter.get("/myprofile/edit/:id", (req, res, next) => {
+  res.render("edit-profile");
 
+  const { id } = req.params;
+
+  User.findById(id)
+    .then((thisUser) => {
+      const data = { thisUser: thisUser };
+      res.render("edit-profile", data);
+    })
+    .catch((err) => {
+      console.log(err);
+      next();
+    });
+});
+
+privateRouter.post("/myprofile/edit/:id", (req, res, next) => {
+  const data = {
+    id: req.params.id,
+    username,
+    email,
+    password,
+  };
+  User.findByIdAndUpdate(req.params.id, data)
+    .then(() => {
+      res.redirect("/myprofile");
+    })
+    .catch((err) => console.log(err));
+});
 //POST /PRIVATE/EDIT-PROFILE sends new data to the server. Renders same page updated
-
-
 
 //GET /PRIVATE/FAVORITES renders favorites view
 
 //POST /PRIVATE/FAVORITES adds new favorite
 
 //POST /PRIVATE/FAVORITES/:ID deletes existing favorite recipe
-
-
 
 //GET /PRIVATE/MYRECIPES renders myrecipes view
 
@@ -32,19 +65,6 @@ router.get('/', function(req, res, next) {
 
 //POST /PRIVATE/MYRECIPES/:ID delete selected myrecipe
 
-
 // POST PRIVATE/RECIPES/:id adds new review and renders the same page (updated)
 
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = router;
+module.exports = privateRouter;
